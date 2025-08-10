05116130676e84a94eba0b5ea63af6dd6a27adeb
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-08-10 22:37:07 +0530

    Update namespace and application ID to match WiFi Security and Analysis App

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.vigilant"
+    namespace = "com.example.wifisecurity"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.1.13356709"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.vigilant"
+        applicationId = "com.example.wifisecurity"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
